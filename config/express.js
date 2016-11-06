410ec87550defb03024c434b0cc5b5bb3d3b7be2
kit jonathan ho <j.hokit80@gmail.com>
2016-11-06 23:01:36 +0800

    set up of passport middleware

--- config/express.js
+++ config/express.js
@@ -16,7 +16,7 @@ module.exports = function() {
   //CORS
   app.use(function(req,res,next){
 		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Methods", "DELETE,PULL,PATCH,PUT");
+		res.header("Access-Control-Allow-Methods", "GET, POST,DELETE,PULL,PATCH,PUT");
 		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		next();
 	});
---
+++ config/security/passport.js
@@ -0,0 +1,42 @@
+var passport = require('passport');
+var LocalStrategy   = require('passport-local').Strategy;
+var User = require('mongoose').model('User');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+//middleware
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, function(req, email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+      if (err) return done(err);
+
+      // If no user is found
+      if (!user) return done(null, false, req.flash('errorMessage', 'No user found.'));
+
+      // Check if the password is correct
+      if (!user.comparePassword(password)) return done(null, false, req.flash('errorMessage', 'Please check your password!'));
+
+      return done(null, user);
+    });
+  }));
+}
+
+//check for authentication
+exports.isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
