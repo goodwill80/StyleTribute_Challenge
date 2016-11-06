9ad919e5ced74d69cd8a4fce4f2e480613141898
kit jonathan ho <j.hokit80@gmail.com>
2016-11-06 22:55:49 +0800

    setup user model schema

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -6,4 +6,26 @@ var UserSchema = new Schema ({
   email: { type: String, unique: true, lowercase: true},
   password: String,
   timestamps: {}
-})
+});
+
+
+// before saving into moongoose. Passing salt to maskoff the password
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// compare password in the database and the one user type in
+UserSchema.methods.comparePassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
+
+mongoose.model('User', UserSchema);
