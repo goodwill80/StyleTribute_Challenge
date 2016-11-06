cac3dc90d299470987fd40adcfba87361d9b73f8
kit jonathan ho <j.hokit80@gmail.com>
2016-11-06 22:39:39 +0800

    added CORS in express.js

--- config/express.js
+++ config/express.js
@@ -13,6 +13,14 @@ var passport = require('passport');
 module.exports = function() {
   var app = express();
 
+  //CORS
+  app.use(function(req,res,next){
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Methods", "DELETE,PULL,PATCH,PUT");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		next();
+	});
+
   //middleware for morgan and compression
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
